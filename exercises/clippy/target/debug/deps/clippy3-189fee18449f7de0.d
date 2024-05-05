331178dd19b9afbe18d001828acdbbd9fd769db3
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-05 15:42:40 +0200

    add exercises

---
+++ exercises/clippy/target/debug/deps/clippy3-189fee18449f7de0.d
@@ -0,0 +1,9 @@
+/Users/rodion/src/rustlings/exercises/clippy/target/debug/deps/libclippy3-189fee18449f7de0.rmeta: clippy3.rs Cargo.toml
+
+/Users/rodion/src/rustlings/exercises/clippy/target/debug/deps/clippy3-189fee18449f7de0.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
